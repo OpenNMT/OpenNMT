0feb89285a016ff068b2c229ef02d94e2f7c9e83
Cyril Tiquet <cyril.tiquet@systrangroup.com>
2016-11-02 18:46:28 +0100

    move options parsing from beam to evaluate script

--- evaluate.lua
+++ evaluate.lua
@@ -1,11 +1,46 @@
+require 'torch'
 local beam = require 's2sa.beam'
 local path = require 'pl.path'
 
-local function main()
-  beam.init(arg)
-  local opt = beam.getOptions()
+local cmd = torch.CmdLine()
 
+-- file location
+cmd:option('-model', 'seq2seq_lstm_attn.t7.', [[Path to model .t7 file]])
+cmd:option('-src_file', '', [[Source sequence to decode (one line per sequence)]])
+cmd:option('-targ_file', '', [[True target sequence (optional)]])
+cmd:option('-output_file', 'pred.txt', [[Path to output the predictions (each line will be the decoded sequence]])
+cmd:option('-src_dict', 'data/demo.src.dict', [[Path to source vocabulary (*.src.dict file)]])
+cmd:option('-targ_dict', 'data/demo.targ.dict', [[Path to target vocabulary (*.targ.dict file)]])
+cmd:option('-feature_dict_prefix', 'data/demo', [[Prefix of the path to features vocabularies (*.feature_N.dict files)]])
+cmd:option('-char_dict', 'data/demo.char.dict', [[If using chars, path to character vocabulary (*.char.dict file)]])
+
+-- beam search options
+cmd:option('-beam', 5, [[Beam size]])
+cmd:option('-max_sent_l', 250, [[Maximum sentence length. If any sequences in srcfile are longer than this then it will error out]])
+cmd:option('-simple', 0, [[If = 1, output prediction is simply the first time the top of the beam
+                         ends with an end-of-sentence token. If = 0, the model considers all
+                         hypotheses that have been generated so far that ends with end-of-sentence
+                         token and takes the highest scoring of all of them.]])
+cmd:option('-replace_unk', 0, [[Replace the generated UNK tokens with the source token that
+                              had the highest attention weight. If srctarg_dict is provided,
+                              it will lookup the identified source token and give the corresponding
+                              target token. If it is not provided (or the identified source token
+                              does not exist in the table) then it will copy the source token]])
+cmd:option('-srctarg_dict', 'data/en-de.dict', [[Path to source-target dictionary to replace UNK
+                                               tokens. See README.md for the format this file should be in]])
+cmd:option('-score_gold', false, [[If = true, score the log likelihood of the gold as well]])
+cmd:option('-n_best', 1, [[If > 1, it will also output an n_best list of decoded sentences]])
+cmd:option('-gpuid', -1, [[ID of the GPU to use (-1 = use CPU)]])
+cmd:option('-gpuid2', -1, [[Second GPU ID]])
+cmd:option('-fallback_to_cpu', false, [[If = true, fallback to CPU if no GPU available]])
+cmd:option('-cudnn', 0, [[If using character model, this should be = 1 if the character model was trained using cudnn]])
+
+
+local function main()
+  local opt = cmd:parse(arg)
   assert(path.exists(opt.src_file), 'src_file does not exist')
+
+  beam.init(opt)
 
   local file = io.open(opt.src_file, "r")
   local out_file = io.open(opt.output_file,'w')
