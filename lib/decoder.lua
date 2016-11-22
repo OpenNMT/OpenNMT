22350a632cc56b1b4a1b99ae4eb1e7e5f204d3cb
Guillaume Klein <guillaume.klein@systrangroup.com>
2016-11-22 18:39:40 +0100

    add masked softmax module

--- lib/decoder.lua
+++ lib/decoder.lua
@@ -3,69 +3,10 @@ local table_utils = require 'lib.utils.table_utils'
 local cuda = require 'lib.utils.cuda'
 require 'lib.sequencer'
 
+require 'lib.MaskedSoftmax'
 
 --[[ Decoder is the sequencer for the target words.]]
 local Decoder, Sequencer = torch.class('Decoder', 'Sequencer')
-
---[[Apply a mask to make sure no attention is given to padding.
-
-Parameters:
-  * `source_sizes` -  the true lengths (with left padding).
-  * `source_length` - the max length in the batch `beam_size`.
-  * `beam_size` - beam size ${K}
-
-Returns:
-  * A nngraph that applies a soft max on input that gives no weight to the left padding.
-
-TODO:
-  * better names for these variables.
---]]
-local function masked_softmax(source_sizes, source_length, beam_size)
-
-  local num_sents = source_sizes:size(1)
-  local input = nn.Identity()()
-  local softmax = nn.SoftMax()(input) -- beam_size*num_sents x State.source_length
-
-  -- Now we are masking the part of the output we don't need
-  local tab
-  if beam_size ~= nil then
-    tab = nn.SplitTable(2)(nn.View(beam_size, num_sents, source_length)(softmax))
-    -- num_sents x { beam_size x State.source_length }
-  else
-    tab = nn.SplitTable(1)(softmax) -- num_sents x { State.source_length }
-  end
-
-  local par = nn.ParallelTable()
-
-  for b = 1, num_sents do
-    local pad_length = source_length - source_sizes[b]
-    local dim = 2
-    if beam_size == nil then
-      dim = 1
-    end
-
-    local seq = nn.Sequential()
-    seq:add(nn.Narrow(dim, pad_length + 1, source_sizes[b]))
-    seq:add(nn.Padding(1, -pad_length, 1, 0))
-    par:add(seq)
-  end
-
-  local out_tab = par(tab) -- num_sents x { beam_size x State.source_length }
-  local output = nn.JoinTable(1)(out_tab) -- num_sents*beam_size x State.source_length
-  if beam_size ~= nil then
-    output = nn.View(num_sents, beam_size, source_length)(output)
-    output = nn.Transpose({1,2})(output) -- beam_size x num_sents x State.source_length
-    output = nn.View(beam_size*num_sents, source_length)(output)
-  else
-    output = nn.View(num_sents, source_length)(output)
-  end
-
-  -- Make sure the vector sums to 1 (softmax output)
-  output = nn.Normalize(1)(output)
-
-  return nn.gModule({input}, {output})
-end
-
 
 function Decoder:__init(args, network)
   Sequencer.__init(self, 'dec', args, network)
@@ -121,7 +62,7 @@ function Decoder:reset(source_sizes, source_length, beam_size)
     if module.name == 'softmax_attn' then
       local mod
       if source_sizes ~= nil then
-        mod = masked_softmax(source_sizes, source_length, beam_size)
+        mod = MaskedSoftmax(source_sizes, source_length, beam_size)
       else
         mod = nn.SoftMax()
       end
