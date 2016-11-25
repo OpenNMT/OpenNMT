8506b1d3eacb96a98b36edd9cd258183bbd4f878
Guillaume Klein <guillaume.klein@systrangroup.com>
2016-11-25 14:51:25 +0100

    add missing files

---
+++ lib/onmt/init.lua
@@ -0,0 +1,16 @@
+require('torch')
+
+onmt = {}
+
+require('lib.onmt.Sequencer')
+require('lib.onmt.Encoder')
+require('lib.onmt.BiEncoder')
+require('lib.onmt.Decoder')
+
+require('lib.onmt.LSTM')
+
+require('lib.onmt.MaskedSoftmax')
+require('lib.onmt.WordEmbedding')
+require('lib.onmt.GlobalAttention')
+
+return onmt
---
+++ lib/utils/init.lua
@@ -0,0 +1,12 @@
+require('torch')
+
+utils = {}
+
+utils.Cuda = require('lib.utils.cuda')
+utils.Dict = require('lib.utils.dict')
+utils.FileReader = require('lib.utils.file_reader')
+utils.Model = require('lib.utils.model_utils')
+utils.Opt = require('lib.utils.opt_utils')
+utils.Table = require('lib.utils.table_utils')
+
+return utils
