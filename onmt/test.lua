766caae39eecfe50cc218c5f3aab758d2923baa8
Guillaume Klein <guillaume.klein@systrangroup.com>
2016-12-14 14:05:38 +0100

    follow got and expected order

--- onmt/test.lua
+++ onmt/test.lua
@@ -48,21 +48,21 @@ local tensorTest = torch.TestSuite()
 function tensorTest.reuseSmaller()
   local a = torch.Tensor(10, 200)
   local b = onmt.utils.Tensor.reuseTensor(a, { 5, 200 })
-  tester:eq(torch.pointer(a:storage()), torch.pointer(b:storage()))
+  tester:eq(torch.pointer(b:storage()), torch.pointer(a:storage()))
 end
 
 function tensorTest.reuseSame()
   local a = torch.Tensor(10, 200)
   local b = onmt.utils.Tensor.reuseTensor(a, a:size())
-  tester:eq(torch.pointer(a:storage()), torch.pointer(b:storage()))
+  tester:eq(torch.pointer(b:storage()), torch.pointer(a:storage()))
 end
 
 function tensorTest.reuseMultipleResize()
   local a = torch.Tensor(10, 200)
   local b = onmt.utils.Tensor.reuseTensor(a, { 5, 200 })
-  tester:eq(torch.pointer(a:storage()), torch.pointer(b:storage()))
+  tester:eq(torch.pointer(b:storage()), torch.pointer(a:storage()))
   local c = onmt.utils.Tensor.reuseTensor(a, { 10, 200 })
-  tester:eq(torch.pointer(a:storage()), torch.pointer(c:storage()))
+  tester:eq(torch.pointer(c:storage()), torch.pointer(a:storage()))
 end
 
 tester:add(tensorTest)
