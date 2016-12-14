d336d0d66a31b88b2cce9d9b98e8e65cb71f0eed
Guillaume Klein <guillaume.klein@systrangroup.com>
2016-12-14 14:25:19 +0100

    add tests for dictionary implementation

--- onmt/test.lua
+++ onmt/test.lua
@@ -68,6 +68,97 @@ end
 tester:add(tensorTest)
 
 
+local dictTest = torch.TestSuite()
+
+function dictTest.createEmptyDict()
+  local d = onmt.utils.Dict.new()
+  tester:eq(#d, 0)
+end
+
+function dictTest.createSimpleDict()
+  local d = onmt.utils.Dict.new()
+  d:add('foo')
+  d:add('bar')
+  d:add('foobar')
+
+  tester:eq(#d, 3)
+
+  tester:ne(d:lookup('foo'), nil)
+  tester:ne(d:lookup('bar'), nil)
+  tester:ne(d:lookup('foobar'), nil)
+end
+
+function dictTest.createSimpleDictWithSpecialTokens()
+  local d = onmt.utils.Dict.new({'toto', 'titi'})
+  d:add('foo')
+  d:add('bar')
+  d:add('foobar')
+
+  tester:eq(#d, 5)
+  tester:eq(#d.special, 2)
+  tester:ne(d:lookup('toto'), nil)
+  tester:ne(d:lookup('titi'), nil)
+end
+
+function dictTest.createSimpleDictDuplicate()
+  local d = onmt.utils.Dict.new()
+  d:add('foo')
+  d:add('bar')
+  d:add('foobar')
+  d:add('bar')
+
+  tester:eq(#d, 3)
+  tester:eq(d.frequencies[d:lookup('bar')], 2)
+end
+
+function dictTest.pruneLarger()
+  local d = onmt.utils.Dict.new()
+  d:add('foo')
+  d:add('bar')
+  d:add('foobar')
+  d:add('bar')
+
+  local pruned = d:prune(5)
+  tester:eq(torch.pointer(pruned), torch.pointer(d))
+end
+
+function dictTest.pruneSmaller()
+  local d = onmt.utils.Dict.new()
+  d:add('foo')
+  d:add('bar')
+  d:add('foobar')
+  d:add('bar')
+  d:add('foobar')
+  d:add('bar')
+
+  local pruned = d:prune(2)
+  tester:eq(#pruned, 2)
+  tester:eq(pruned:lookup('foo'), nil)
+  tester:ne(pruned:lookup('bar'), nil)
+  tester:ne(pruned:lookup('foobar'), nil)
+end
+
+function dictTest.pruneSmallerWithSpecialTokens()
+  local d = onmt.utils.Dict.new({ 'toto', 'titi' })
+  d:add('foo')
+  d:add('bar')
+  d:add('foobar')
+  d:add('bar')
+  d:add('foobar')
+  d:add('bar')
+
+  local pruned = d:prune(2)
+  tester:eq(#pruned, 2 + 2)
+  tester:eq(pruned:lookup('foo'), nil)
+  tester:ne(pruned:lookup('bar'), nil)
+  tester:ne(pruned:lookup('foobar'), nil)
+  tester:ne(pruned:lookup('toto'), nil)
+  tester:ne(pruned:lookup('titi'), nil)
+end
+
+tester:add(dictTest)
+
+
 local nmttest = torch.TestSuite()
 
 -- local function equal(t1, t2, msg)
