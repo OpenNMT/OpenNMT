86c3599e9a6fc72dbfcdddd8be1a1c1918a8805a
Guillaume Klein <guillaume.klein@systrangroup.com>
2016-12-19 14:29:36 +0100

    add test for invariable special tokens index in dictionary

--- onmt/test.lua
+++ onmt/test.lua
@@ -156,6 +156,23 @@ function dictTest.pruneSmallerWithSpecialTokens()
   tester:ne(pruned:lookup('titi'), nil)
 end
 
+function dictTest.pruneInvariableSpecialTokensIndex()
+  local d = onmt.utils.Dict.new({ 'toto', 'titi' })
+  d:add('foo')
+  d:add('bar')
+  d:add('foobar')
+  d:add('bar')
+  d:add('foobar')
+  d:add('bar')
+
+  local totoIndex = d:lookup('toto')
+  local titiIndex = d:lookup('titi')
+
+  local pruned = d:prune(2)
+  tester:eq(pruned:lookup('toto'), totoIndex)
+  tester:eq(pruned:lookup('titi'), titiIndex)
+end
+
 tester:add(dictTest)
 
 
