21da4c69f2e889136fd1a0739ec1da964f9baf1d
Yuntian <da03@users.noreply.github.com>
2016-12-19 22:45:10 -0500

    Update Log.lua

--- onmt/utils/Log.lua
+++ onmt/utils/Log.lua
@@ -1,3 +1,6 @@
+require 'class'
+require 'paths'
+
 local function logJsonRecursive(obj)
   if type(obj) == 'string' then
     io.write('"' .. obj .. '"')
@@ -26,6 +29,89 @@ end
 local function logJson(obj)
   logJsonRecursive(obj)
   io.write('\n')
+end
+
+
+--[[ logging is a class used for maintaining logs in a log file.
+--]]
+local logging = torch.class('onmt.logging')
+
+--[[ Construct a logging object.
+Parameters:
+  * `logPath` - the path to log file.
+  * `mute` - whether or not outputs to screen.
+]]
+function logging:__init(logPath, mute)
+  mute = mute or false
+  self.mute = mute
+  local openMode = 'w'
+  if paths.filep(logPath) then
+    local input = nil
+    while not input do
+      print('Logging file exits. Overwrite(o)? Append(a)? Abort(q)?')
+      input = io.read()
+      if input == 'o' or input == 'O' then
+        openMode = 'w'
+      elseif input == 'a' or input == 'A' then
+        openMode = 'a'
+      elseif input == 'q' or input == 'Q' then
+        os.exit()
+      else
+        openMode = 'a'
+      end
+    end
+  end
+  self.logFile = io.open(logPath, openMode)
+end
+
+--[[ Log a message at a specified level.
+Parameters:
+  * `message` - the message to log.
+  * `level` - the desired message level. ['INFO']
+]]
+function logging:log(message, level)
+  level = level or 'INFO'
+  local timeStamp = os.date('%x %X')
+  local msgFormatted = string.format('[%s %s] %s', timeStamp, level, message)
+  if not self.mute then
+    print (msgFormatted)
+  end
+  if self.logFile then
+    self.logFile:write(msgFormatted .. '\n')
+    self.logFile:flush()
+  end
+end
+
+--[[ Log a message at 'INFO' level.
+Parameters:
+  * `message` - the message to log. Supports formatting string.
+]]
+function logging:info(...)
+  self:log(string.format(...), 'INFO')
+end
+
+--[[ Log a message at 'WARNING' level.
+Parameters:
+  * `message` - the message to log. Supports formatting string.
+]]
+function logging:warning(...)
+  self:log(string.format(...), 'WARNING')
+end
+
+--[[ Log a message at 'ERROR' level.
+Parameters:
+  * `message` - the message to log. Supports formatting string.
+]]
+function logging:error(...)
+  self:log(string.format(...), 'ERROR')
+end
+
+--[[ Deconstructor. Close the log file.
+]]
+function logging:shutDown()
+  if self.logFile then
+    self.logFile:close()
+  end
 end
 
 return {
