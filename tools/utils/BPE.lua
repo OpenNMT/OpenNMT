01cc54f6119bb14ebeff1290ec2133a55d01e470
Yongchao DENG <yongchao.deng@systrangroup.com>
2016-12-22 19:31:46 +0100

    first import bpe

---
+++ tools/utils/BPE.lua
@@ -0,0 +1,107 @@
+local unicode = require './unicode'
+
+local BPE = torch.class('BPE')
+
+function BPE:__init(codesfile_path)
+  local codes = {}
+  local f = assert(io.open(codesfile_path, "r"))
+  local t=f:read("*line")
+  local i=1
+
+  while not(t == nil) do
+    l=string.split(t," ")
+    if (#l==2) then
+      codes[t]=i
+      i=i+1
+    end
+    t=f:read("*line")
+  end
+  self.codes = codes
+end
+
+local function getPairs(word)
+  local pairs = {}
+  for i=1, #word-1, 1 do
+    table.insert(pairs, word[i]..' '..word[i+1])
+  end
+  return pairs
+end
+
+local function str2word(l)
+  local word = {}
+  for v, c in unicode.utf8_iter(l) do
+    table.insert(word, c)
+  end
+  table.insert(word, '</w>')
+  return word
+end
+
+function BPE:minPair(pairsTable)
+  mintmp = 100000
+  minpair = ''
+  for i=1, #pairsTable, 1 do
+    pair_cur = pairsTable[i]
+    if (self.codes[pair_cur] ~= nil) then
+      scoretmp = self.codes[pair_cur]
+      if (scoretmp < mintmp) then
+        mintmp = scoretmp
+        minpair = pair_cur
+      end
+    end
+  end
+  return minpair
+end
+
+function BPE:encode(l)
+  local nextv, nextc = unicode._utf8_to_cp(l, 1)
+  if #l <= #nextc then
+    return l
+  end
+  local word = str2word(l)
+  local pairs = getPairs(word)
+  print (pairs)
+  while (true) do
+    local bigram = self:minPair(pairs)
+    if (bigram == '') then
+      break
+    end
+    bigram = string.split(bigram," ")
+    local new_word = {}
+    local merge = false
+    for ii, xx in ipairs(word) do
+      if (merge) then
+        if (bigram[2] == xx) then
+          table.insert(new_word, bigram[1]..bigram[2])
+        else
+          table.insert(new_word, bigram[1])
+	  end
+        merge = false
+      else
+        if (bigram[1] == xx ) then
+          merge = true
+        else
+          table.insert(new_word, xx)
+        end
+      end
+    end
+    word = new_word
+    if #word == 1 then
+      break
+    else
+      pairs = getPairs(word)
+    end
+  end
+  if word[#word] == '</w>' then
+    table.remove(word, #word)
+  elseif (string.sub(word[#word],-string.len('</w>'))=='</w>') then
+    word[#word] = string.sub(word[#word], 1, string.len('</w>'))
+  end
+  output = ''
+  for i = 1, #word-1, 1 do
+    output = output..word[i]..'@@ '
+  end
+  output = output..word[#word]
+  return output
+end
+
+return BPE
