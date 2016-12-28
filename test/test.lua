4bc8a43b79bf7bec18cf449d67650ade2b4d5406
Guillaume Klein <guillaume.klein@systrangroup.com>
2016-12-28 15:41:12 +0100

    revert features separator escaping logic
    It completes tokenization changes introduced in 481b7847.
    
    It reverts changes introduced in e0bfee0, e24c378f and 68597d35.

--- test/test.lua
+++ test/test.lua
@@ -6,37 +6,37 @@ local tester = torch.Tester()
 local stringTest = torch.TestSuite()
 
 function stringTest.noSplit()
-  tester:eq(onmt.utils.String.split('foo-foo', '\\|'), { 'foo-foo' })
+  tester:eq(onmt.utils.String.split('foo-foo', '￨'), { 'foo-foo' })
 end
 function stringTest.emptySplit2()
-  tester:eq(onmt.utils.String.split('\\|', '\\|'), { '', '' })
+  tester:eq(onmt.utils.String.split('￨', '￨'), { '', '' })
 end
 function stringTest.emptySplit1Right()
-  tester:eq(onmt.utils.String.split('foo\\|', '\\|'), { 'foo', '' })
+  tester:eq(onmt.utils.String.split('foo￨', '￨'), { 'foo', '' })
 end
 function stringTest.emptySplit1Middle()
-  tester:eq(onmt.utils.String.split('foo\\|\\|bar', '\\|'), { 'foo', '', 'bar' })
+  tester:eq(onmt.utils.String.split('foo￨￨bar', '￨'), { 'foo', '', 'bar' })
 end
 function stringTest.emptySplit1Left()
-  tester:eq(onmt.utils.String.split('\\|foo', '\\|'), { '', 'foo' })
+  tester:eq(onmt.utils.String.split('￨foo', '￨'), { '', 'foo' })
 end
 function stringTest.split2()
-  tester:eq(onmt.utils.String.split('foo\\|bar', '\\|'), { 'foo', 'bar' })
+  tester:eq(onmt.utils.String.split('foo￨bar', '￨'), { 'foo', 'bar' })
 end
 function stringTest.split3()
-  tester:eq(onmt.utils.String.split('foo\\|bar\\|foobar', '\\|'), { 'foo', 'bar', 'foobar' })
+  tester:eq(onmt.utils.String.split('foo￨bar￨foobar', '￨'), { 'foo', 'bar', 'foobar' })
 end
-function stringTest.doNotSplitEscaped1()
-  tester:eq(onmt.utils.String.split('foo\\\\|bar', '\\|'), { 'foo\\\\|bar' })
+function stringTest.ignoreEscaping1()
+  tester:eq(onmt.utils.String.split('foo\\￨bar', '￨'), { 'foo\\', 'bar' })
 end
-function stringTest.doNotSplitEscaped2()
-  tester:eq(onmt.utils.String.split('foo\\\\|bar\\|foobar', '\\|'), { 'foo\\\\|bar', 'foobar' })
+function stringTest.ignoreEscaping2()
+  tester:eq(onmt.utils.String.split('foo\\￨bar￨foobar', '￨'), { 'foo\\', 'bar', 'foobar' })
 end
-function stringTest.doNotSplitEscaped3()
-  tester:eq(onmt.utils.String.split('\\\\|', '\\|'), { '\\\\|' })
+function stringTest.ignoreEscaping3()
+  tester:eq(onmt.utils.String.split('\\￨', '￨'), { '\\', '' })
 end
-function stringTest.escapedEscapedSeparator()
-  tester:eq(onmt.utils.String.split('\\\\\\|N', '\\|'), { '\\\\', 'N' })
+function stringTest.ignoreEscaping4()
+  tester:eq(onmt.utils.String.split('\\\\￨N', '￨'), { '\\\\', 'N' })
 end
 
 function stringTest.noStrip()
