eddc2e504709fb6b3eb7bbd1cc57e40134c5bd55
Ubuntu <dengyuntian@gmail.com>
2017-01-05 23:52:14 +0000

    refactoring

--- onmt/translate/BeamSearchAdvancer.lua
+++ onmt/translate/BeamSearchAdvancer.lua
@@ -1,18 +1,18 @@
 --[[ Class for specifying how to advance one step
 
-
-           ______________
-          / (update beam)\
-         /                \
-      scores, states       \     .................    nil, nil
-        /                   \    /                 \     /
-    step()                   \ step()               \  step()
-       /                      \ /                    \ /
-  nil, nil---------------tokens, states........tokens, states
-
-      |                        |                      |
-
-    t = 1--------------------t = 2..............t = max seq len
+  -- Initialize states
+  extensions, states <-- init()
+  while number of remaining hypotheses > 0 do
+    -- Update states
+    states <-- forward(extensions, states)
+    -- Extend hypotheses by one step and return the scores
+    scores <-- expand(states)
+    -- Update beam (maintained by BeamSearcher)
+    extensions <-- _updateBeam()
+    for b = 1, batchSize do
+      if isFinal(hypotheses)[b] then
+        -- Remove completed hypotheses (maintained by BeamSearcher)
+        _removeHypotheses(b)
 
  ==================================================================
 
@@ -20,43 +20,72 @@ Speicifies how to go one step forward.
 --]]
 local BeamSearchAdvancer = torch.class('BeamSearchAdvancer')
 
---[[Step function. BeamSearcher will take care of updating beams and
-  transforming states, such that this function only need to take a batched input
-  and produce a batched output. All tensors (either parameters or returns,
-  including tensors inside tables) must have the same first dimension `batchSize`.
-  Note that since BeamSearcher will remove completed hypotheses, the `batchSize`
-  does not remain between steps, hence this function cannot assume a static one.
-
-Parameters:
-
-  * `tokens` - a flat tensor of size `batchSize`, predictions from the previous step. For the first step, it would be nil.
-  * `states` - the states from the previous step. For the first step, it would be nil.
+--[[Initialize function. Returns initial inputs for the forward function.
 
 Returns:
 
-  * `scores` - a 2D tensor of size `(batchSize, vocabSize)`, scores for the current step. For the last step, return nil.
-  * `states` - the states for the current step. For the last step, return nil.
+  * `extensions` - the initial extensions if necessary.
+  * `states` - the initial states.
 
 ]]
-function BeamSearchAdvancer:step(tokens, states)
-  local scores, statesOut, t
-  if tokens == nil then
-    -- The first step, batchSize 3, vocabSize 5, hiddenSize 7 and 8
-    t = 1
-    scores = torch.zeros(3, 5)
-    statesOut = {torch.zeros(3, 7), torch.zeros(3, 8), 1}
-  else
-    -- The following steps, must obtain batchSize from tokens
-    local batchSize = tokens:size(1)
-    t = states[3]
-    scores = torch.zeros(batchSize, 5)
-    statesOut = {torch.zeros(batchSize, 7), torch.zeros(batchSize, 8), t + 1}
-  end
-  if t <= 10 then
-    return scores, statesOut
-  else
-    return nil, nil
-  end
+function BeamSearchAdvancer:init()
+end
+
+--[[Forward function. Update states given extensions from the previous step.
+
+Parameters:
+
+  * `extensions` - a flat tensor of size `batchSize`, the selected extension indexes from the previous step.
+  * `states` - a table containing the states from the previous step.
+
+Returns:
+
+  * `states` - a table containing the updated states.
+
+]]
+function BeamSearchAdvancer:forward(extensions, states)
+end
+
+--[[Expand function. Given states, returns the scores of the possible 
+  `extensionSize` extensions.
+
+Parameters:
+
+  * `states` - the current states.
+
+Returns:
+
+  * `scores` - a 2D tensor of size `(batchSize, extensionSize)`.
+
+]]
+function BeamSearchAdvancer:expand(states)
+end
+
+--[[Determines which hypotheses are final.
+
+Parameters:
+
+  * `hypotheses` - a table. `hypotheses[t]` is a tensor of size `(batchSize, extensionSize)`, indicating the hypotheses at step `t`.
+  * `states` - a table of current states.
+
+Returns: a binary flat tensor of size `(batchSize)`, indicating which hypotheses are complete.
+
+]]
+function BeamSearchAdvancer:isFinal(hypotheses, states)
+end
+
+
+--[[Determines which hypotheses shall be pruned.
+--
+Parameters:
+
+  * `hypotheses` - a table. `hypotheses[t]` is a tensor of size `(batchSize, extensionSize)`, indicating the hypotheses at step `t`.
+  * `states` - a table of current states.
+
+Returns: a binary flat tensor of size `(batchSize)`, indicating which hypotheses shall be pruned.
+
+]]
+function BeamSearchAdvancer:filter(hypotheses, states)
 end
 
 --[[Specifies which states to keep track of. After beam search, those states
@@ -69,6 +98,16 @@ Parameters:
 ]]
 function BeamSearchAdvancer:setKeptStateIndexes(indexes)
   self.keptStateIndexes = indexes
+end
+
+-- Private function, not recommended to overwrite.
+function BeamSearchAdvancer:_step(extensions, states)
+  if states == nil then
+    extensions, states = self:init()
+  end
+  states = self:forward(extensions, states)
+  local scores = self:extend(states)
+  return scores, states
 end
 
 return BeamSearchAdvancer
