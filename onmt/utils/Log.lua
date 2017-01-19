79ebd0b307b4695f9b03e9dd65b5ce633ed936c3
Yuntian <da03@users.noreply.github.com>
2017-01-19 04:29:47 -0500

    Fix JSON logging (#82)
    It fixes #81.

--- onmt/utils/Log.lua
+++ onmt/utils/Log.lua
@@ -1,31 +1,31 @@
 local function logJsonRecursive(obj)
   if type(obj) == 'string' then
-    _G.logger:write('"' .. obj .. '"')
+    _G.logger:writeMsg('"' .. obj .. '"')
   elseif type(obj) == 'table' then
     local first = true
 
-    _G.logger:write('{')
+    _G.logger:writeMsg('{')
 
     for key, val in pairs(obj) do
       if not first then
-        _G.logger:write(',')
+        _G.logger:writeMsg(',')
       else
         first = false
       end
-      _G.logger:write('"' .. key .. '":')
+      _G.logger:writeMsg('"' .. key .. '":')
       logJsonRecursive(val)
     end
 
-    _G.logger:write('}')
+    _G.logger:writeMsg('}')
   else
-    _G.logger:write(tostring(obj))
+    _G.logger:writeMsg(tostring(obj))
   end
 end
 
 --[[ Recursively outputs a Lua object to a JSON objects followed by a new line. ]]
 local function logJson(obj)
   logJsonRecursive(obj)
-  _G.logger:write('\n')
+  _G.logger:writeMsg('\n')
 end
 
 return {
