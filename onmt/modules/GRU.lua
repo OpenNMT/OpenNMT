5aea6bd68443f1221dc199ee210d602737aa63f5
Jean A. Senellart <jean.senellart@systrangroup.com>
2017-01-21 10:32:34 +0100

    improve documentation

--- onmt/modules/GRU.lua
+++ onmt/modules/GRU.lua
@@ -25,11 +25,11 @@ local GRU, parent = torch.class('onmt.GRU', 'onmt.Network')
 --[[
 Parameters:
 
-  * `layers` - Number of LSTM layers, L.
+  * `layers` - Number of layers
   * `inputSize` - Size of input layer
-  * `hiddenSize` - Size of the hidden layers.
-  * `dropout` - Dropout rate to use.
-  * `residual` - Residual connections between layers.
+  * `hiddenSize` - Size of the hidden layers
+  * `dropout` - Dropout rate to use (should be in $$[0,1]$$ range.
+  * `residual` - Residual connections between layers (boolean)
 --]]
 function GRU:__init(layers, inputSize, hiddenSize, dropout, residual)
   dropout = dropout or 0
@@ -93,14 +93,21 @@ end
             r_t = sigmoid(W_{xr} x_t + b_{xr} + W_{hr} h_{(t-1)} + b_{hr}) \\
             i_t = sigmoid(W_{xi} x_t + b_{xi} + W_hi h_{(t-1)} + b_{hi}) \\
             n_t = \tanh(W_{xn} x_t + b_{xn} + r_t * (W_{hn} h_{(t-1)} + b_{hn}) \\
-            h_t = (1 - i_t) * n_t + i_t * h_{(t-1)} = n_t + i_t * (h_{(t-1)-n}) \\
+            h_t = (1 - i_t) * n_t + i_t * h_{(t-1)} = n_t + i_t * (h_{(t-1) - n}) \\
             \end{array}
 
-    where :math:`h_t` is the hidden state at time `t`, :math:`x_t` is the hidden
-    state of the previous layer at time `t` or :math:`input_t` for the first layer,
-    and :math:`r_t`, :math:`i_t`, :math:`n_t` are the reset, input, and new gates, respectively.
+    where $$h_t` is the hidden state at time `t`, $$x_t$$ is the hidden
+    state of the previous layer at time `t` or $$input_t$$ for the first layer,
+    and $$r_t$$, $$i_t$$, $$n_t$$ are the reset, input, and new gates, respectively.
 
-    In the function `prevH`=:math:`h_{(t-1}}`, `nextH`=:math:`h_t`
+    In the function: `prevH`=$$h_{(t-1}}$$, `nextH`=$$h_t$$, `r`=$$r_t$$, `i`=$$i_t$$,
+    `n`=$$n_t$$.
+
+    `x2h_r,x2h_i,x2h_n` are $$W_{xr},W_{xi},W_{xn}$$ and the biases $$b_{xr},b_{xi},b_{xn}$$.
+
+    And `h2h_r,h2h_i,h2h_n` are $$W_{hr},W_{hi},W_{hn}$$ and the biases $$b_{hr},b_{hi},b_{hn}$$.
+
+
 ]]
 function GRU:_buildLayer(inputSize, hiddenSize)
   local inputs = {}
