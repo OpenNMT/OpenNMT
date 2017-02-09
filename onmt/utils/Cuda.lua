a5480b3ad751df58354258a765b0b492221dd682
Guillaume Klein <guillaume.klein@systrangroup.com>
2017-02-09 09:58:32 +0100

    Fix typo

--- onmt/utils/Cuda.lua
+++ onmt/utils/Cuda.lua
@@ -60,7 +60,7 @@ function Cuda.convert(obj)
       else
         return obj:cuda()
       end
-    elseif not Cuda.activated and obj.float ~= nil and then
+    elseif not Cuda.activated and obj.float ~= nil then
       -- Defaults to float instead of double.
       if objtype:find('torch%..*LongTensor') then
         return obj:long()
