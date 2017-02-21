76ad2d0d4a43064abeee8dc5ba001c3bda94856c
Guillaume Klein <guillaume.klein@systrangroup.com>
2017-02-21 15:29:25 +0100

    Rename build_dict to build_vocab

--- build_vocab.lua
+++ build_vocab.lua
@@ -1,15 +1,15 @@
 require('onmt.init')
 
-local cmd = onmt.utils.ExtendedCmdLine.new('build_dict.lua')
+local cmd = onmt.utils.ExtendedCmdLine.new('build_vocab.lua')
 
 local options = {
   {'-data', '', 'Data file', {valid=onmt.utils.ExtendedCmdLine.fileExists}},
-  {'-save_dict', '', 'Dictionary file', {valid=onmt.utils.ExtendedCmdLine.nonEmpty}},
+  {'-save_vocab', '', 'Vocabulary files name', {valid=onmt.utils.ExtendedCmdLine.nonEmpty}},
   {'-vocab_size', '50000', 'Comma-separated list of target vocabularies size: word[,feat1,feat2,...].',
    {valid=onmt.utils.ExtendedCmdLine.listUInt}}
 }
 
-cmd:setCmdLineOptions(options, 'Dictionary creation')
+cmd:setCmdLineOptions(options, 'Vocabulary creation')
 onmt.utils.Logger.declareOpts(cmd)
 
 local function isValid(sent)
@@ -23,8 +23,8 @@ local function main()
 
   local vocab = onmt.data.Vocabulary.init('source', opt.data, '', opt.vocab_size, '', isValid)
 
-  onmt.data.Vocabulary.save('source', vocab.words, opt.save_dict .. '.dict')
-  onmt.data.Vocabulary.saveFeatures('source', vocab.features, opt.save_dict)
+  onmt.data.Vocabulary.save('source', vocab.words, opt.save_vocab .. '.dict')
+  onmt.data.Vocabulary.saveFeatures('source', vocab.features, opt.save_vocab)
 
   _G.logger:shutDown()
 end
