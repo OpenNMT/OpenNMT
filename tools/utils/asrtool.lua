b6f1b11c8a420cbb1e9920136d21e9885c821703
Jean A. Senellart <jean.senellart@systrangroup.com>
2017-02-24 08:26:32 +0900

    finalize feature extraction

--- tools/utils/asrtool.lua
+++ tools/utils/asrtool.lua
@@ -1,5 +1,6 @@
 local asrtool = torch.class('asrtool')
 local signal = require 'signal'
+local audio = require 'audio'
 
 -- MFSC and MFCC calculation inspired from
 -- https://github.com/jameslyons/python_speech_features
@@ -7,6 +8,7 @@ local signal = require 'signal'
 
 -- Options declaration.
 local options = {
+  { 'feats',  'mfcc', [[Features to extract]], { enum={'mfcc','mfsc'}}},
   { 'winlen', 0.025, [[The length of the analysis window in seconds.]] },
   { 'winstep', 0.01, [[the step between successive windows in seconds.]] },
   { 'wintype', 'rect', [[Windows type.]],
@@ -19,7 +21,9 @@ local options = {
   { 'ceplifter', 22, [[Apply a lifter to final cepstral coefficients. 0 is no lifter. ]] },
   { 'appendEnergy', 1, [[If non zeoro, the zeroth cepstral coefficient is replaced
                          with the log of the total frame energy.]] },
-  { 'cmvn', 1, [[If non zero, apply cepstral mean and variance normalization.]]}
+  { 'cmvn', 1, [[If non zero, apply cepstral mean and variance normalization.]]},
+  { 'delta_step',   5, [[Value of N in delta calculation, 0 to disable delta calculation.]],
+          { valid=onmt.utils.ExtendedCmdLine.isInt(0)}}
 }
 
 function asrtool.declareOpts(cmd)
@@ -87,6 +91,29 @@ local function get_filterbanks(n, winsize, lowfreq, highfreq, samplerate)
   return fbank
 end
 
+--[[Compute delta features from a feature vector sequence.
+
+  * feat: a tensor of size (NUMFRAMES by number of features) containing features. Each row holds 1 feature vector.
+  * N: For each frame, calculate delta features based on preceding and following N frames
+  * returns a tensor containing delta features. Each row holds 1 delta feature vector.
+]]
+local function delta(feat, N)
+  local NUMFRAMES = feat:size(1)
+  local denominator = 0
+  for i=1, N do
+    denominator = denominator + 2*i*i
+  end
+  local delta_feat = torch.zeros(feat:size(1), feat:size(2))
+  local padded = torch.Tensor(feat:size(1)+2*N, feat:size(2))
+  for i=1,N do
+    padded:narrow(1, i, 1):copy(feat:narrow(1,1,1))
+    padded:narrow(1, N+i+feat:size(2), 1):copy(feat:narrow(1, feat:size(1), 1))
+  end
+  for t=1, NUMFRAMES do
+    delta_feat[t] = (torch.linspace(-N, N+1, 2*N+1):resize(1,2*N+1)*padded:narrow(1, t, 2*N+1)) / denominator
+  end
+  return delta_feat
+end
 
 --[[Apply a cepstral lifter to the matrix of cepstra. T
     This has the effect of increasing the
@@ -129,6 +156,11 @@ function asrtool:mfcc(saudio, samplerate)
     feats = (feats - mean)/std
   end
 
+  if self.args.delta_step > 0 then
+    local deltaf = delta(feats, self.args.delta_step)
+    local delta2f = delta(deltaf, self.args.delta_step)
+    feats = torch.cat({feats, deltaf, delta2f})
+  end
   return feats
 end
 
@@ -165,6 +197,14 @@ function asrtool:mfsc(saudio, samplerate)
   local feat = pspec*fb:t()
 
   return energy, feat
+end
+
+function asrtool:extractFeats(saudio, samplerate)
+  if self.args.feats == 'mfcc' then
+    return self:mfcc(saudio, samplerate)
+  else
+    return self:mfsc(saudio, samplerate)
+  end
 end
 
 return asrtool
