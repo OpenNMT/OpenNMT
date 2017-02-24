6f056d58897f83e645bd128eaff1f9996a07628c
Jean A. Senellart <jean.senellart@systrangroup.com>
2017-02-24 16:34:51 +0900

    help

--- tools/utils/audiotool.lua
+++ tools/utils/audiotool.lua
@@ -8,21 +8,22 @@ local audio = require 'audio'
 
 -- Options declaration.
 local options = {
-  { 'feats',   'mfcc', [[Features to extract]], { enum={'mfcc','mfsc'}} },
-  { 'winlen',   0.025, [[The length of the analysis window in seconds.]] },
-  { 'winstep',  0.010, [[the step between successive windows in seconds.]] },
-  { 'wintype', 'rect', [[Windows type.]],
+  { '-feats',   'mfcc', [[Features to extract.]], { enum={'mfcc','mfsc'}} },
+  { '-winlen',   0.025, [[The length of the analysis window in seconds.]] },
+  { '-winstep',  0.010, [[the step between successive windows in seconds.]] },
+  { '-wintype', 'rect', [[Windows type.]],
                   { enum={'rect', 'hamming', 'hann', 'bartlett'}} },
-  { 'numcep',      13, [[The number of cepstrum to return.]] },
-  { 'nfilt',       26, [[The number of filters in the filterbank.]] },
-  { 'lowfreq',      0, [[Lowest band edge of mel filters in herz.]] },
-  { 'highfreq',    -1, [[Highest band edge of mel filters in Hz, if negative default to samplerate/2]] },
-  { 'preemph',   0.97, [[Apply preemphasis filter with preemph as coefficient. 0 is no filter.]] },
-  { 'ceplifter',   22, [[Apply a lifter to final cepstral coefficients. 0 is no lifter. ]] },
-  { 'appendEnergy', 1, [[If non zeoro, the zeroth cepstral coefficient is replaced
+  { '-numcep',      13, [[The number of cepstrum to return.]] },
+  { '-nfilt',       26, [[The number of filters in the filterbank.]] },
+  { '-lowfreq',      0, [[Lowest band edge of mel filters in herz.]] },
+  { '-highfreq',    -1, [[Highest band edge of mel filters in Hz, if negative default to samplerate/2.]] },
+  { '-preemph',   0.97, [[Apply preemphasis filter with preemph as coefficient. 0 is no filter.]] },
+  { '-ceplifter',   22, [[Apply a lifter to final cepstral coefficients. 0 is no lifter. ]] },
+  { '-appendEnergy', 1, [[If non zero, the zeroth cepstral coefficient is replaced
                               with the log of the total frame energy.]] },
-  { 'cmvn',         1, [[If non zero, apply cepstral mean and variance normalization.]]},
-  { 'delta_step',   5, [[Value of N in delta calculation, 0 to disable delta calculation.]],
+  { '-cmvn',         1, [[If non zero, apply cepstral mean and variance normalization.]],
+                  { valid=onmt.utils.ExtendedCmdLine.isInt(0,1)} },
+  { '-delta_step',   5, [[Value of N in delta calculation, 0 to disable delta calculation.]],
                   { valid=onmt.utils.ExtendedCmdLine.isInt(0)} }
 }
 
