6d5da5ef170ca08cdce58e39f151243989e26c4b
Jungi Kim <jungi.kim@gmail.com>
2017-03-02 18:28:17 +0100

    iterate on 08ad2ed (Refactor SampledDataset options handling)

--- test/test.lua
+++ test/test.lua
@@ -307,11 +307,13 @@ function SampledDatasetTest.Sample()
   _G.logger = onmt.utils.Logger.new(nil, true, nil)
 
   local dataSize = 1234
-  local samplingSize = 100
   local batchSize = 16
-  local sample_w_ppl = false
-  local sample_w_ppl_init = 100
-  local sample_w_ppl_max = 1000
+
+  local opt = {}
+  opt.sample = 100
+  opt.sample_w_ppl = false
+  opt.sample_w_ppl_init = 100
+  opt.sample_w_ppl_max = 1000
 
   local tds = require('tds')
   local srcData = {words = tds.Vec(), features = tds.Vec()}
@@ -319,56 +321,46 @@ function SampledDatasetTest.Sample()
   for i = 1, dataSize do
     srcData.words:insert(torch.Tensor(5))
     srcData.features:insert(tds.Vec())
-    srcData.features[1]:insert(torch.Tensor(5))
+    srcData.features[i]:insert(torch.Tensor(5))
     tgtData.words:insert(torch.Tensor(5))
     tgtData.features:insert(tds.Vec())
-    tgtData.features[1]:insert(torch.Tensor(5))
+    tgtData.features[i]:insert(torch.Tensor(5))
   end
 
-  tester:eq(#srcData.words, dataSize)
-
-  local dataset = onmt.data.SampledDataset.new(srcData, tgtData, samplingSize, sample_w_ppl, sample_w_ppl_init, sample_w_ppl_max)
+  -- random sampling
+  local dataset = onmt.data.SampledDataset.new(srcData, tgtData, opt)
   dataset:setBatchSize(batchSize)
 
   local numSampled = dataset:getNumSampled()
-  local numBatch = math.ceil(numSampled / batchSize)
 
-  tester:eq(dataset:getBatch(1).size, 16)
-  tester:eq(dataset:getNumSampled(), samplingSize)
-  tester:eq(dataset:batchCount(), numBatch)
+  tester:eq(dataset:getNumSampled(), opt.sample)
   for i = 1, dataset:batchCount() do
     dataset:getBatch(i)
   end
 
-  sample_w_ppl = true
-  dataset = onmt.data.SampledDataset.new(srcData, tgtData, samplingSize, sample_w_ppl, sample_w_ppl_init, sample_w_ppl_max)
+  -- sampling with ppl
+  opt.sample_w_ppl = true
+
+  dataset = onmt.data.SampledDataset.new(srcData, tgtData, opt)
   dataset:setBatchSize(batchSize)
 
   numSampled = dataset:getNumSampled()
-  numBatch = math.ceil(numSampled / batchSize)
 
-  tester:eq(dataset:getBatch(1).size, 16)
-  tester:eq(dataset:getNumSampled(), samplingSize)
-  tester:eq(dataset:batchCount(), numBatch)
-
+  tester:eq(dataset:getNumSampled(), opt.sample)
   for i = 1, dataset:batchCount() do
     dataset:getBatch(i)
   end
 
   -- oversampling
-  samplingSize = 2000
-  sample_w_ppl = false
+  opt.sample = 2000
+  opt.sample_w_ppl = false
 
-  dataset = onmt.data.SampledDataset.new(srcData, tgtData, samplingSize, sample_w_ppl, sample_w_ppl_init, sample_w_ppl_max)
+  dataset = onmt.data.SampledDataset.new(srcData, tgtData, opt)
   dataset:setBatchSize(batchSize)
 
   numSampled = dataset:getNumSampled()
-  numBatch = math.ceil(numSampled / batchSize)
 
-  tester:eq(dataset:getBatch(1).size, 16)
-  tester:eq(dataset:getNumSampled(), samplingSize)
-  tester:eq(dataset:batchCount(), numBatch)
-
+  tester:eq(dataset:getNumSampled(), opt.sample)
   for i = 1, dataset:batchCount() do
     dataset:getBatch(i)
   end
