80cb2a83a59fab0e099188a951cccb32fd5f5a47
Guillaume Klein <guillaume.klein@systrangroup.com>
2017-03-08 08:51:18 -0800

    Add warning about the CUDA caching allocator

--- onmt/utils/Cuda.lua
+++ onmt/utils/Cuda.lua
@@ -48,6 +48,10 @@ function Cuda.init(opt, masterGPU)
         _G.logger:info('Using GPU(s): ' .. table.concat(Cuda.gpuIds, ', '))
       end
 
+      if cutorch.isCachingAllocatorEnabled and cutorch.isCachingAllocatorEnabled() then
+        _G.logger:warning('The caching CUDA memory allocator is enabled. This allocator improves performance at the cost of a higher GPU memory usage. To optimize for memory, consider disabling it by setting the environment variable: THC_CACHING_ALLOCATOR=0')
+      end
+
       cutorch.setDevice(Cuda.gpuIds[masterGPU])
 
       if opt.seed then
