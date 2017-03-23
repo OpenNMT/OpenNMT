86f72116347bd5e62f7cecaa8950c745bdb7a5c6
pnquan90 <ngoc.pham@kit.edu>
2017-03-23 14:53:37 +0100

    fixed Decoder and added bin files

---
+++ bin/preprocess.lua
@@ -0,0 +1,113 @@
+require('onmt.init')
+
+local cmd = onmt.utils.ExtendedCmdLine.new('preprocess.lua')
+
+-- First argument define the dataType: bitext/monotext - default is bitext.
+local dataType = cmd.getArgument(arg, '-data_type') or 'bitext'
+
+-- Options declaration
+local options = {
+  {'-data_type',         'bitext',  [[Type of text to preprocess. Use 'monotext' for monolingual text.
+                                    This option impacts all options choices.]],
+                                    {enum={'bitext','monotext'}}},
+  {'-save_data',               '',  [[Output file for the prepared data]],
+                                    {valid=onmt.utils.ExtendedCmdLine.nonEmpty}}
+}
+
+cmd:setCmdLineOptions(options, 'Preprocess')
+
+onmt.data.Preprocessor.declareOpts(cmd, dataType)
+
+local otherOptions = {
+  {'-seed',                   3425,    [[Random seed]],
+                                   {valid=onmt.utils.ExtendedCmdLine.isUInt()}},
+  {'-report_every',           100000,  [[Report status every this many sentences]],
+                                   {valid=onmt.utils.ExtendedCmdLine.isUInt()}}
+}
+cmd:setCmdLineOptions(otherOptions, 'Other')
+onmt.utils.Logger.declareOpts(cmd)
+
+local opt = cmd:parse(arg)
+
+local function isValid(sent, maxSeqLength)
+  return #sent > 0 and #sent <= maxSeqLength
+end
+
+local function main()
+
+  torch.manualSeed(opt.seed)
+
+  _G.logger = onmt.utils.Logger.new(opt.log_file, opt.disable_logs, opt.log_level)
+
+  local Vocabulary = onmt.data.Vocabulary
+  local Preprocessor = onmt.data.Preprocessor.new(opt, dataType)
+
+  local data = { dataType=dataType }
+
+  data.dicts = {}
+  data.dicts.src = Vocabulary.init('train',
+                                   opt.train_src or opt.train,
+                                   opt.src_vocab or opt.vocab,
+                                   opt.src_vocab_size or opt.vocab_size,
+                                   opt.features_vocabs_prefix,
+                                   function(s) return isValid(s, opt.src_seq_length or opt.seq_length) end)
+  if dataType ~= 'monotext' then
+    data.dicts.tgt = Vocabulary.init('target',
+                                     opt.train_tgt,
+                                     opt.tgt_vocab,
+                                     opt.tgt_vocab_size,
+                                     opt.features_vocabs_prefix,
+                                     function(s) return isValid(s, opt.tgt_seq_length) end)
+  end
+
+  _G.logger:info('Preparing training data...')
+  data.train = {}
+  if dataType == 'monotext' then
+    data.train.src = Preprocessor:makeMonolingualData(opt.train, data.dicts.src, isValid)
+  else
+    data.train.src, data.train.tgt = Preprocessor:makeBilingualData(opt.train_src, opt.train_tgt,
+                                                                    data.dicts.src, data.dicts.tgt,
+                                                                    isValid)
+  end
+
+  _G.logger:info('')
+
+  _G.logger:info('Preparing validation data...')
+  data.valid = {}
+  if dataType == 'monotext' then
+    data.valid.src = Preprocessor:makeMonolingualData(opt.valid, data.dicts.src, isValid)
+  else
+    data.valid.src, data.valid.tgt = Preprocessor:makeBilingualData(opt.valid_src, opt.valid_tgt,
+                                                                    data.dicts.src, data.dicts.tgt,
+                                                                    isValid)
+  end
+
+  _G.logger:info('')
+
+  if dataType == 'monotext' then
+    if opt.vocab:len() == 0 then
+      Vocabulary.save('source', data.dicts.src.words, opt.save_data .. '.dict')
+    end
+    if opt.features_vocabs_prefix:len() == 0 then
+      Vocabulary.saveFeatures('source', data.dicts.src.features, opt.save_data)
+    end
+  else
+    if opt.src_vocab:len() == 0 then
+      Vocabulary.save('source', data.dicts.src.words, opt.save_data .. '.src.dict')
+    end
+
+    if opt.tgt_vocab:len() == 0 then
+      Vocabulary.save('target', data.dicts.tgt.words, opt.save_data .. '.tgt.dict')
+    end
+    if opt.features_vocabs_prefix:len() == 0 then
+      Vocabulary.saveFeatures('source', data.dicts.src.features, opt.save_data)
+      Vocabulary.saveFeatures('target', data.dicts.tgt.features, opt.save_data)
+    end
+  end
+
+  _G.logger:info('Saving data to \'' .. opt.save_data .. '-train.t7\'...')
+  torch.save(opt.save_data .. '-train.t7', data, 'binary', false)
+  _G.logger:shutDown()
+end
+
+main()
---
+++ bin/translate.lua
@@ -0,0 +1,169 @@
+require('onmt.init')
+
+local cmd = onmt.utils.ExtendedCmdLine.new('translate.lua')
+
+local options = {
+  {'-src', '', [[Source sequence to decode (one line per sequence)]],
+               {valid=onmt.utils.ExtendedCmdLine.nonEmpty}},
+  {'-tgt', '', [[True target sequence (optional)]]},
+  {'-output', 'pred.txt', [[Path to output the predictions (each line will be the decoded sequence)]]}
+}
+
+cmd:setCmdLineOptions(options, 'Data')
+
+onmt.translate.Translator.declareOpts(cmd)
+
+cmd:text('')
+cmd:text('**Other options**')
+cmd:text('')
+
+cmd:option('-time', false, [[Measure batch translation time]])
+
+onmt.utils.Cuda.declareOpts(cmd)
+onmt.utils.Logger.declareOpts(cmd)
+
+local function reportScore(name, scoreTotal, wordsTotal)
+  _G.logger:info(name .. " AVG SCORE: %.2f, " .. name .. " PPL: %.2f",
+                 scoreTotal / wordsTotal,
+                 math.exp(-scoreTotal/wordsTotal))
+end
+
+local function main()
+  local opt = cmd:parse(arg)
+
+  _G.logger = onmt.utils.Logger.new(opt.log_file, opt.disable_logs, opt.log_level)
+
+  local srcReader = onmt.utils.FileReader.new(opt.src)
+  local srcBatch = {}
+
+  local goldReader
+  local goldBatch
+
+  local withGoldScore = opt.tgt:len() > 0
+
+  if withGoldScore then
+    goldReader = onmt.utils.FileReader.new(opt.tgt)
+    goldBatch = {}
+  end
+
+  local translator = onmt.translate.Translator.new(opt)
+
+  local outFile = io.open(opt.output, 'w')
+
+  local sentId = 1
+  local batchId = 1
+
+  local predScoreTotal = 0
+  local predWordsTotal = 0
+  local goldScoreTotal = 0
+  local goldWordsTotal = 0
+
+  local timer
+  if opt.time then
+    timer = torch.Timer()
+    timer:stop()
+    timer:reset()
+  end
+
+  while true do
+    local srcTokens = srcReader:next()
+    local goldTokens
+    if withGoldScore then
+      goldTokens = goldReader:next()
+    end
+
+    if srcTokens ~= nil then
+      table.insert(srcBatch, translator:buildInput(srcTokens))
+
+      if withGoldScore then
+        table.insert(goldBatch, translator:buildInput(goldTokens))
+      end
+    elseif #srcBatch == 0 then
+      break
+    end
+
+    if srcTokens == nil or #srcBatch == opt.batch_size then
+      if opt.time then
+        timer:resume()
+      end
+
+      local results = translator:translate(srcBatch, goldBatch)
+
+      if opt.time then
+        timer:stop()
+      end
+
+      for b = 1, #results do
+        if (#srcBatch[b].words == 0) then
+          _G.logger:warning('Line ' .. sentId .. ' is empty.')
+          outFile:write('\n')
+        else
+          _G.logger:info('SENT %d: %s', sentId, translator:buildOutput(srcBatch[b]))
+
+          if withGoldScore then
+            _G.logger:info('GOLD %d: %s', sentId, translator:buildOutput(goldBatch[b]), results[b].goldScore)
+            _G.logger:info("GOLD SCORE: %.2f", results[b].goldScore)
+            goldScoreTotal = goldScoreTotal + results[b].goldScore
+            goldWordsTotal = goldWordsTotal + #goldBatch[b].words
+          end
+
+          for n = 1, #results[b].preds do
+            local sentence = translator:buildOutput(results[b].preds[n])
+
+            if n == 1 then
+              outFile:write(sentence .. '\n')
+              predScoreTotal = predScoreTotal + results[b].preds[n].score
+              predWordsTotal = predWordsTotal + #results[b].preds[n].words
+
+              if #results[b].preds > 1 then
+                _G.logger:info('')
+                _G.logger:info('BEST HYP:')
+              end
+            end
+
+            if #results[b].preds > 1 then
+              _G.logger:info("[%.2f] %s", results[b].preds[n].score, sentence)
+            else
+              _G.logger:info("PRED %d: %s", sentId, sentence)
+              _G.logger:info("PRED SCORE: %.2f", results[b].preds[n].score)
+            end
+          end
+        end
+
+        _G.logger:info('')
+        sentId = sentId + 1
+      end
+
+      if srcTokens == nil then
+        break
+      end
+
+      batchId = batchId + 1
+      srcBatch = {}
+      if withGoldScore then
+        goldBatch = {}
+      end
+      collectgarbage()
+    end
+  end
+
+  if opt.time then
+    local time = timer:time()
+    local sentenceCount = sentId-1
+    _G.logger:info("Average sentence translation time (in seconds):\n")
+    _G.logger:info("avg real\t" .. time.real / sentenceCount .. "\n")
+    _G.logger:info("avg user\t" .. time.user / sentenceCount .. "\n")
+    _G.logger:info("avg sys\t" .. time.sys / sentenceCount .. "\n")
+  end
+
+  reportScore('PRED', predScoreTotal, predWordsTotal)
+
+  if withGoldScore then
+    reportScore('GOLD', goldScoreTotal, goldWordsTotal)
+  end
+
+  outFile:close()
+  _G.logger:shutDown()
+end
+
+main()
