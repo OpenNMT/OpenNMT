cdb84237101e61ce0d09753e68bab37a0165251c
Guillaume Klein <guillaume.klein@systrangroup.com>
2017-04-06 14:18:36 +0200

    Fix LanguageModel training
    Fixes #173.

--- onmt/LanguageModel.lua
+++ onmt/LanguageModel.lua
@@ -62,8 +62,8 @@ function LanguageModel:__init(args, dicts, verbose)
   parent.__init(self, args)
   onmt.utils.Table.merge(self.args, onmt.utils.ExtendedCmdLine.getModuleOpts(args, options))
 
-  self.models.encoder = onmt.Factory.buildWordEncoder(self.args, dicts.src, verbose)
-  self.models.generator = onmt.Factory.buildGenerator(self.args.rnn_size, dicts.src)
+  self.models.encoder = onmt.Factory.buildWordEncoder(args, dicts.src, verbose)
+  self.models.generator = onmt.Factory.buildGenerator(args.rnn_size, dicts.src)
 
   self.criterion = onmt.ParallelClassNLLCriterion(onmt.Factory.getOutputSizes(dicts.src))
 
