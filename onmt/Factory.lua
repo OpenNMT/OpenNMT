c1bc6c1294beccf83b7fbb93713f881fdf8e34c8
Jean A. Senellart <jean.senellart@systrangroup.com>
2017-04-06 11:37:51 +0200

    rename 'no attention'

--- onmt/Factory.lua
+++ onmt/Factory.lua
@@ -26,7 +26,7 @@ local options = {
     '-attention', 'global',
     [[Attention model.]],
     {
-      enum = {'no', 'global'}
+      enum = {'none', 'global'}
     }
   }
 }
@@ -254,7 +254,7 @@ function Factory.buildGenerator(rnnSize, dicts)
 end
 
 function Factory.buildAttention(args)
-  if args.attention == 'no' then
+  if args.attention == 'none' then
     _G.logger:info('   - No Attention')
     return onmt.NoAttention
   else
