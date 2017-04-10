509c12df2f3eb1915b7f6aac35a881225e602c61
Guillaume Klein <guillaume.klein@systrangroup.com>
2017-04-10 11:32:00 +0200

    Add more string tests

--- test/onmt/StringTest.lua
+++ test/onmt/StringTest.lua
@@ -4,6 +4,9 @@ local tester = ...
 
 local stringTest = torch.TestSuite()
 
+function stringTest.split_empty()
+  tester:eq(onmt.utils.String.split('', '￨'), { })
+end
 function stringTest.split_noSplit()
   tester:eq(onmt.utils.String.split('foo-foo', '￨'), { 'foo-foo' })
 end
@@ -38,6 +41,9 @@ function stringTest.split_ignoreEscaping4()
   tester:eq(onmt.utils.String.split('\\\\￨N', '￨'), { '\\\\', 'N' })
 end
 
+function stringTest.strip_empty()
+  tester:eq(onmt.utils.String.strip(''), '')
+end
 function stringTest.strip_noStrip()
   tester:eq(onmt.utils.String.strip('foo'), 'foo')
 end
@@ -49,6 +55,32 @@ function stringTest.strip_stripRight()
 end
 function stringTest.strip_stripBoth()
   tester:eq(onmt.utils.String.strip('    foo  '), 'foo')
+end
+
+function stringTest.stripHyphens_empty()
+  tester:eq(onmt.utils.String.stripHyphens(''), '')
+end
+function stringTest.stripHyphens_none()
+  tester:eq(onmt.utils.String.stripHyphens('toto'), 'toto')
+end
+function stringTest.stripHyphens_one()
+  tester:eq(onmt.utils.String.stripHyphens('-toto'), 'toto')
+end
+function stringTest.stripHyphens_many()
+  tester:eq(onmt.utils.String.stripHyphens('-----toto'), 'toto')
+end
+
+function stringTest.pad_emptyNoPad()
+  tester:eq(onmt.utils.String.pad('', 0), '')
+end
+function stringTest.pad_emptyPadded()
+  tester:eq(onmt.utils.String.pad('', 2), '  ')
+end
+function stringTest.pad_nonEmptyNoPad()
+  tester:eq(onmt.utils.String.pad('toto', 2), 'toto')
+end
+function stringTest.pad_nonEmptyPadded()
+  tester:eq(onmt.utils.String.pad('toto', 6), 'toto  ')
 end
 
 return stringTest
