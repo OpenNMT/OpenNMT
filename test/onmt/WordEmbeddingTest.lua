7a97d7ac8da8e4fea568d7c7f1bbe3f27faad4fe
Guillaume Klein <guillaume.klein@systrangroup.com>
2017-04-14 16:43:01 +0200

    Complete WordEmbedding tests

--- test/onmt/WordEmbeddingTest.lua
+++ test/onmt/WordEmbeddingTest.lua
@@ -7,9 +7,13 @@ local wordEmbeddingTest = torch.TestSuite()
 function wordEmbeddingTest.default()
   local emb = onmt.WordEmbedding(10, 5)
   local input = torch.LongTensor({{1, 2}, {3, 4}, {5, 6}})
-  local output = emb:forward(input)
+  local gradOutput = torch.Tensor(3, 2, 5):uniform()
 
+  local output = emb:forward(input)
   tester:eq(output:size(), torch.LongStorage({3, 2, 5}))
+
+  local gradInput = emb:backward(input, gradOutput)
+  tester:eq(gradInput:size(), torch.LongStorage({3, 2}))
 end
 
 
@@ -37,6 +41,20 @@ function wordEmbeddingTest.fixedToogled()
   local emb = onmt.WordEmbedding(10, 5)
   emb:fixEmbeddings(true)
   testFixedEmb(emb)
+  emb:fixEmbeddings(false)
+  tester:ne(emb.net.gradWeight, nil)
+end
+
+function wordEmbeddingTest.pretrained()
+  local embs = torch.Tensor(10, 5):uniform()
+  torch.save('embs.t7', embs)
+
+  local emb = onmt.WordEmbedding(10, 5, 'embs.t7')
+  emb:postParametersInitialization()
+
+  tester:eq(emb.net.weight:narrow(1, 2, 9), embs:narrow(1, 2, 9))
+
+  os.remove('embs.t7')
 end
 
 return wordEmbeddingTest
