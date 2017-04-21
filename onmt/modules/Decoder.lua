619cf401dd1f7f8745d0d383f53a7a11090d2be5
Jean A. Senellart <jean.senellart@systrangroup.com>
2017-04-21 11:16:40 +0200

    save coverage size in decoder args

--- onmt/modules/Decoder.lua
+++ onmt/modules/Decoder.lua
@@ -68,6 +68,7 @@ function Decoder:__init(args, inputNetwork, generator, attentionModel)
 
   self.args.inputFeed = (args.input_feed == 1)
   self.args.hasCoverage = attentionModel.hasCoverage
+  self.args.coverage_size = attentionModel.coverage_size
 
   parent.__init(self, self:_buildModel(attentionModel))
 
@@ -348,7 +349,7 @@ function Decoder:initializeSpecialStates(states, _, batch)
   -- if need coverage, initialize it
   if self.args.hasCoverage then
     states.coverage = onmt.utils.Tensor.reuseTensor(self.coverageProto,
-                                                           { batch.size, batch.encoderOutputLength or batch.sourceLength, self.attnLayer.coverageSize })
+                                                           { batch.size, batch.encoderOutputLength or batch.sourceLength, self.args.coverageSize })
   end
 end
 
@@ -442,7 +443,7 @@ function Decoder:backward(batch, outputs, criterion)
   local indvAvgLoss = torch.zeros(outputs[1]:size(1))
 
   if self.args.hasCoverage then
-    gradStatesInput[#gradStatesInput]:resize(batch.size, batch.encoderOutputLength or batch.sourceLength, self.attnLayer.coverageSize)
+    gradStatesInput[#gradStatesInput]:resize(batch.size, batch.encoderOutputLength or batch.sourceLength, self.args.coverageSize)
   end
 
   for t = batch.targetLength, 1, -1 do
