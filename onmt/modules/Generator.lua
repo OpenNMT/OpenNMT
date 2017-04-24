c4ac91bb0a25a0720a296bbedb840e0f54ab6dda
Jean A. Senellart <jean.senellart@systrangroup.com>
2017-04-24 17:26:46 +0200

    convert NCE<>NLL

--- onmt/modules/Generator.lua
+++ onmt/modules/Generator.lua
@@ -6,10 +6,31 @@ local Generator, parent = torch.class('onmt.Generator', 'onmt.Network')
 
 
 function Generator:__init(opt, dicts, sizes)
-  parent.__init(self, self:_buildGenerator(opt, dicts, sizes))
+  parent.__init(self)
+  self:buildGenerator(opt, dicts, sizes)
 end
 
-function Generator:_buildGenerator(opt, dicts, sizes)
+--[[ Build or convert a generator - if convert is true, keep the current generator linear parameters.]]
+function Generator:buildGenerator(opt, dicts, sizes, convert)
+  local rnn_size = opt.rnn_size
+  if convert then
+    local prevgen = (self.needOutput and 'NCE') or 'NLL'
+    local newgen = (opt.criterion == 'nce' and 'NCE') or 'NLL'
+    if newgen == prevgen then
+      return
+    end
+    _G.logger:info(' * Converting '..prevgen..' criterion into '..newgen)
+    if not sizes then
+      sizes = {}
+      for i = 1, #self.net.modules do
+        local m = self.net.modules[i]
+        -- m is a Sequential - m.modules[1] is a selector, m.modules[2] is NCEModule or Linear
+        table.insert(sizes, m.modules[2].weight:size(1))
+        rnn_size = m.modules[2].weight:size(2)
+      end
+    end
+  end
+
   local generator = nn.ConcatTable()
 
   local selectInput = nn.Identity()
@@ -17,10 +38,13 @@ function Generator:_buildGenerator(opt, dicts, sizes)
   if opt.criterion == 'nce' then
     self.needOutput = true
     selectInput = nn.SelectTable(1)
+  else
+    self.needOutput = false
   end
 
   for i = 1, #sizes do
     local feat_generator
+    local linmod
     if i == 1 and opt.criterion == 'nce' then
       assert(dicts.words.freqTensor, "missing frequencies in dictionary - use -keep_frequency in preprocess.lua")
       assert(onmt.NCEModule, "missing NCE module - install dpnn torch libraries")
@@ -28,44 +52,32 @@ function Generator:_buildGenerator(opt, dicts, sizes)
                           :add(nn.SelectTable(1)) -- first element is the input
                           :add(nn.Sequential():add(nn.SelectTable(2)):add(nn.SelectTable(i)))
 
+      linmod = onmt.NCEModule(opt, rnn_size, sizes[i], dicts.words.freqTensor)
+
       feat_generator = nn.Sequential()
                     :add(selectInputOutput)
-                    :add(onmt.NCEModule(opt, opt.rnn_size, sizes[i], dicts.words.freqTensor))
+                    :add(linmod)
+
     else
+      linmod = nn.Linear(rnn_size, sizes[i])
       feat_generator = nn.Sequential()
                     :add(selectInput)
-                    :add(nn.Linear(opt.rnn_size, sizes[i]))
+                    :add(linmod)
                     :add(nn.LogSoftMax())
+    end
+    if convert then
+      linmod.weight = self.net.modules[i].modules[2].weight
+      linmod.bias = self.net.modules[i].modules[2].bias
     end
     generator:add(feat_generator)
   end
-  return generator
-end
-
---[[ Convert from NCE to regular LogSoftMax ]]
-function Generator:convertToLogSoftMax()
-  _G.logger:info(' * Converting NCE module into regular linear/softmax')
-  local generator = nn.ConcatTable()
-  for i = 1, #self.net.modules do
-    local m = self.net.modules[i]
-    -- m is a Sequential - m.modules[1] is a selector, m.modules[2] is NCEModule or Linear
-    local linear = nn.Linear(m.modules[2].weight:size(2),m.modules[2].weight:size(1))
-    linear.weight = m.modules[2].weight
-    linear.bias = m.modules[2].bias
-    local feat_generator = nn.Sequential()
-                        :add(linear)
-                        :add(nn.LogSoftMax())
-    generator:add(feat_generator)
-  end
   self:set(generator)
-  self.needOutput = false
 end
 
---[[ Release Generator - if NCE convert to Linear/Logsoftmax. ]]
+--[[ Release Generator - ie. if NCE convert to Linear/Logsoftmax. ]]
 function Generator:release()
-  if self.needOutput then
-    self:convertToLogSoftMax()
-  end
+  local opt = { criterion='nll' }
+  self:buildGenerator(opt, nil, nil, true)
 end
 
 function Generator:updateOutput(input)
