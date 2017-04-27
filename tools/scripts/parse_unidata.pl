ca35419aa6b05075c93f9c4124fff1e705043126
Jean A. Senellart <jean.senellart@systrangroup.com>
2017-04-27 16:55:11 +0200

    save Unidata parsing script

---
+++ tools/scripts/parse_unidata.pl
@@ -0,0 +1,91 @@
+#!/bin/perl
+
+# script to generate unidata.lua from UnicodeData.txt
+
+use strict;
+use utf8;
+
+print <<___HEADER;
+--[[ Class containing unicode data table summarized information]]
+local unidata = {}
+unidata.maplower = {}
+
+-- the following sections have been generated from UnicodeData.txt and should not be modified manually
+___HEADER
+
+my @Number;
+my @Separator;
+my @LetterLower;
+my @LetterUpper;
+my @LetterOther;
+my @Mark;
+my %maplower;
+
+while(my $l=<STDIN>) {
+  #0043;LATIN CAPITAL LETTER C;Lu;0;L;;;;;N;;;;0063;
+  my ($codepoint,$name,$gc,$ccc,$bidicat,$cdm,$dgvalue,$dvalue,$nvalue,$mirrored,$u1name,$comment,$umap,$lmap,$tmap,$other1)=split(/;/,$l);
+  die $l if (!defined $tmap or defined $other1);
+  my $cp=eval "0x$codepoint";
+  last if ($cp>65536);
+  if ($gc=~/N/) { $Number[$cp]=1; }
+  elsif ($gc=~/Z/) { $Separator[$cp]=1; }
+  elsif ($gc=~/Ll/) { $LetterLower[$cp]=1; }
+  elsif ($gc=~/Lu/) { $LetterUpper[$cp]=1; }
+  elsif ($gc=~/L/) { $LetterOther[$cp]=1; }
+  elsif ($gc=~/M/) { $Mark[$cp]=1; }
+  if ($gc=~/L/ && $lmap ne "") {
+    my $lower=eval "0x$lmap";
+    $maplower{$cp}=$lower;
+  }
+}
+
+generate("Number",@Number);
+generate("Separator",@Separator);
+generate("LetterLower",@LetterLower);
+generate("LetterUpper",@LetterUpper);
+generate("LetterOther",@LetterOther);
+generate("Mark",@Mark);
+
+foreach my $cp (sort {$a<=>$b} keys(%maplower)) {
+  print "unidata.maplower[".ghex($cp)."]=".ghex($maplower{$cp})."\n";
+}
+
+print "\nreturn unidata\n";
+
+sub generate {
+  my ($s,@t)=@_;
+  print "\nunidata.".$s." = {}\n";
+  for(my $i=32; $i<=65536; $i++) {
+    if ($t[$i]) {
+      my $last=$i;
+      my $j=$i+1;
+      while($j-$last<=256) {
+        $last=$j if ($t[$j]);
+        $j++;
+      }
+      print "unidata.".$s."[".ghex($i)."] = {";
+      my $h=$i;
+      my $C=0;
+      my $firstline=1;
+      while($h<=$last) {
+        my $V=0;
+        for(my $c=0;$c<16;$c++) {
+          $V=$V<<1;
+          $V=$V+1 if ($t[$h+$c]);
+        }
+        if ($C==0) { print "," if (!$firstline); print "\n  "; } else { print ", "; }
+        print ghex($V);
+        $C++;
+        if ($C==10) { $C=0; $firstline=0; }
+        $h=$h+16;
+      }
+      print "\n}\n";
+      $i=$j;
+    }
+  }
+}
+
+sub ghex {
+  my ($v)=@_;
+  return "0x".uc(sprintf("%04x",$v));
+}
