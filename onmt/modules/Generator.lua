dc5affce2d7aec4461d8e0f8e2a0ef93f81bdd5f
Jean A. Senellart <jean.senellart@systrangroup.com>
2017-05-02 10:36:24 +0200

    fix generator versioning after 41e59d6

--- onmt/modules/Generator.lua
+++ onmt/modules/Generator.lua
@@ -1,6 +1,8 @@
 --[[ Default decoder generator. Given RNN state, produce categorical distribution for tokens and features
 
      Simply implements $$softmax(W h b)$$.
+
+     version 2: merge FeaturesGenerator and Generator - the generator nn is a table
 --]]
 local Generator, parent = torch.class('onmt.Generator', 'onmt.Network')
 
@@ -10,6 +12,8 @@ torch.class('onmt.FeaturesGenerator', 'onmt.Network')
 function Generator:__init(opt, sizes)
   parent.__init(self)
   self:_buildGenerator(opt, sizes)
+  -- for backward compatibility with previous model
+  self.version = 2
 end
 
 function Generator:_buildGenerator(opt, sizes)
@@ -32,15 +36,27 @@ function Generator:release()
 end
 
 function Generator:updateOutput(input)
-  self.output = self.net:updateOutput(input)
+  if not self.version or self.version < 2 then
+    self.output = { self.net:updateOutput(input) }
+  else
+    self.output = self.net:updateOutput(input)
+  end
   return self.output
 end
 
 function Generator:updateGradInput(input, gradOutput)
-  self.gradInput = self.net:updateGradInput(input, gradOutput)
+  if not self.version or self.version < 2 then
+    self.gradInput = self.net:updateGradInput(input, gradOutput[1])
+  else
+    self.gradInput = self.net:updateGradInput(input, gradOutput)
+  end
   return self.gradInput
 end
 
 function Generator:accGradParameters(input, gradOutput, scale)
-  self.net:accGradParameters(input, gradOutput, scale)
+  if not self.version or self.version < 2 then
+    self.net:accGradParameters(input, gradOutput[1], scale)
+  else
+    self.net:accGradParameters(input, gradOutput, scale)
+  end
 end
