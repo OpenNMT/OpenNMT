342867256b9d7e7036e8a468c365933dbe03a128
Jean A. Senellart <jean.senellart@systrangroup.com>
2017-05-05 10:55:10 +0200

    smaller testset

--- test/onmt/RunTest.lua
+++ test/onmt/RunTest.lua
@@ -16,14 +16,14 @@ end
 function runTest.run_real()
   local file = io.popen(TH..[[ preprocess.lua -train_src data/src-train.txt -train_tgt data/tgt-train.txt\
                           -valid_src data/src-val.txt -valid_tgt data/tgt-val.txt -save_data tiny\
-                          -src_vocab_size 50 -tgt_vocab_size 50 -src_seq_length 20 -tgt_seq_length 15\
+                          -src_vocab_size 50 -tgt_vocab_size 50 -src_seq_length 10 -tgt_seq_length 10\
                           2>&1]])
   local output = file:read('*all')
   local status = file:close()
   tester:assert(status ~= nil, "preprocess failed")
   if status then
     local v = tonumber(string.match(output, "Prepared (%d+) sentences"))
-    tester:eq(v, 2984)
+    tester:eq(v, 881)
 
     file = io.popen(TH..[[ train.lua -data tiny-train.t7 -save_model tiny -end_epoch 2\
                       -rnn_size 10 -word_vec_size 10 -profiler 2>&1]])
@@ -42,7 +42,7 @@ function runTest.run_real()
       end
 
       tester:eq(#ppls,2)
-      tester:eq(ppls,{9.95,7.19},1)
+      tester:eq(ppls,{7.99,10.39},1)
       tester:assert(string.find(output, "INFO] profile:") > 0, "cannot find profiling information")
     end
   end
