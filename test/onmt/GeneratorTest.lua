93d8887de5e203cce8006650f1c08bd34df9611f
Jean A. Senellart <jean.senellart@systrangroup.com>
2017-05-05 16:06:32 +0200

    debug test

--- test/onmt/GeneratorTest.lua
+++ test/onmt/GeneratorTest.lua
@@ -69,6 +69,8 @@ function generatorTest.GeneratorIS()
   generator:setTargetVoc(torch.LongTensor{1,2})
   local output_ri = generator:forward({context, torch.Tensor{3}})
 
+  print('output',output[1])
+  print('output_ri',output_ri[1])
   tester:eq(output[1]:narrow(2,1,2), output_ri[1]:narrow(2,1,2))
   tester:eq(output[2], output_ri[2])
   tester:eq(output_ri[1]:size(2),2)
