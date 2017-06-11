9448d3b7abca7923335d8486ee3314d143673645
vince62s <vince62s@yahoo.com>
2017-06-11 18:36:15 +0200

    fix args

--- tools/rest_multi_models.lua
+++ tools/rest_multi_models.lua
@@ -18,50 +18,70 @@ Usage
 
 require('onmt.init')
 
+local yaml = require 'yaml'
+
 local tokenizer = require('tools.utils.tokenizer')
 local BPE = require ('tools.utils.BPE')
 local restserver = require('tools.restserver.restserver')
 
---[[
-  TODO
-  The following should be in a YAML file instead.
-  First item contains the port of the REST server
-]]
+local cmd = onmt.utils.ExtendedCmdLine.new('rest_multi_models.lua')
 
-local server_cfg = {
+local server_options = {
    {
-     {'-model', '/nmt/NMTModels/fr-en/model-fren_epoch10_4.10.t7', [[Model path]]},
-     {'-replace_unk' , true, [[If set replace unk words]]},
-     {'-port', '7784', [[Port to run the server on.]]},
-     {'-withAttn', false, [[If set returns by default attn vector.]]}
-   }
-   ,
+     '-port', 7784,
+     [[Port to run the server on.]],
+     {
+       valid = onmt.utils.ExtendedCmdLine.isUInt
+     }
+   },
    {
-     {'-model', '/nmt/NMTModels/en-fr/model-enfr_epoch10_3.42.t7', [[Model path]]},
-     {'-replace_unk' , true, [[If set replace unk words]]},
-     {'-withAttn', false, [[If set returns by default attn vector.]]}
+     '-withAttn', false,
+     [[If set returns by default attn vector.]]
+   },
+   {
+     '-unload_time', 10,
+     [[Unload unused model from memory after this time.]]
+   },
+   { 
+     '-model_config', 'tools/rest_config.yml',
+     [[Path to yaml configuration file.]],
+     {
+       valid = onmt.utils.ExtendedCmdLine.fileExists
+     }
    }
 }
 
+cmd:setCmdLineOptions(server_options, 'Server')
+onmt.utils.Cuda.declareOpts(cmd)
+onmt.utils.Logger.declareOpts(cmd)
+
+local opt_server = cmd:parse(arg)
+local fconfig = io.open(opt_server.model_config, "rb")
+assert(fconfig)
+local content_config = fconfig:read "*a"
+fconfig:close()
+
+local server_cfg = yaml.load(content_config)
+
 -- CAREFULL opt is a table of options here, ie a table of what opt is usually in onmt
 
-local cmd = {}
 local opt = {}
 
 for i=1, #server_cfg do
-  cmd[i] = onmt.utils.ExtendedCmdLine.new('rest_translation_server.lua')
-  cmd[i]:setCmdLineOptions(server_cfg[i], 'Server')
-  onmt.translate.Translator.declareOpts(cmd[i])
-  onmt.utils.Cuda.declareOpts(cmd[i])
-  onmt.utils.Logger.declareOpts(cmd[i])
-  tokenizer.declareOpts(cmd[i])
+  local model_cmd = onmt.utils.ExtendedCmdLine.new()
+  local model_config = {}
+  for k,v in pairs(server_cfg[i]) do
+    table.insert(model_config, {'-'..k, v, ''})
+  end
+  model_cmd:setCmdLineOptions(model_config, 'Server')
+  onmt.translate.Translator.declareOpts(model_cmd)
+  tokenizer.declareOpts(model_cmd)
 
-  cmd[i]:text("")
-  cmd[i]:text("**Other options**")
-  cmd[i]:text("")
-  cmd[i]:option('-batchsize', 1000, [[Size of each parallel batch - you should not change except if low memory.]])
-
-  opt[i] = cmd[i]:parse(arg)
+  model_cmd:text("")
+  model_cmd:text("**Other options**")
+  model_cmd:text("")
+  model_cmd:option('-batchsize', 1000, [[Size of each parallel batch - you should not change except if low memory.]])
+  opt[i] = model_cmd:parse({})
 end
 
 
@@ -110,7 +130,7 @@ local function translateMessage(server,lines)
   local translations = {}
   for b = 1, #lines do
     local ret = {}
-    for i = 1, translator.opt.n_best do
+    for i = 1, translator.args.n_best do
       local srcSent = translator:buildOutput(batch[b])
       local predSent = translator:buildOutput(results[b].preds[i])
 
@@ -145,7 +165,7 @@ local function translateMessage(server,lines)
 end
 
 local function init_server(options)
-  local server = restserver:new():port(options[1].port)
+  local server = restserver:new():port(opt_server.port)
 
   server.opt = options
   server:add_resource("translator", {
@@ -156,16 +176,16 @@ local function init_server(options)
       produces = "application/json",
       handler = function(req)
         _G.logger:info("receiving request for model id %d",req[1].id)
- -- remember the first item contains also the id of the engine
- -- for backward compatibility maybe if req[1].id not defined, set it to 1
+        -- remember the first item contains also the id of the engine
+        -- for backward compatibility maybe if req[1].id not defined, set it to 1
         if req[1].id == nil then
            req[1].id = 1
         end
         if not server.model_loaded[req[1].id] then
- -- TODO
- -- I need to test here I f I have enough memory to load the model
- -- if not then I need to unload the oldest one
- -- print(cutorch.getMemoryUsage())
+          -- TODO
+          -- I need to test here I f I have enough memory to load the model
+          -- if not then I need to unload the oldest one
+          -- print(cutorch.getMemoryUsage())
           _G.logger:info("Loading model id %d",req[1].id)
           server.translator[req[1].id] = onmt.translate.Translator.new(server.opt[req[1].id])
           server.model_loaded[req[1].id] = true
@@ -184,8 +204,7 @@ end
 local function is_finished(server)
    for i=1, #server_cfg do
      server.elapsed[i] = server.timer[i]:time().real
---     print("Model %d %s time elapsed %d",i,server.model_loaded[i],server.elapsed[i])
-     if server.elapsed[i] > 10 then
+     if server.elapsed[i] > opt_server.unload_time then
        if server.model_loaded[i] then
          print("unloading model %d",i)
          server.translator[i] = nil
@@ -202,10 +221,9 @@ end
 
 local function main()
   -- load logger
-  _G.logger = onmt.utils.Logger.new(nil, false, 'INFO')
-
-  onmt.utils.Cuda.init(opt[1])
-
+  _G.logger = onmt.utils.Logger.new(opt_server.log_file, opt_server.disable_logs, opt_server.log_level)
+  -- cuda settings
+  onmt.utils.Cuda.init(opt_server)
   -- disable profiling
   _G.profiler = onmt.utils.Profiler.new(false)
 
@@ -224,4 +242,5 @@ local function main()
 end
 
 main()
+
 
