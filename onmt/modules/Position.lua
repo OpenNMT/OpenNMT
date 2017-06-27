80ccf83d376650137edaa5806ec8b13ca6b210c9
Natalia Segal <natalia.segal@systrangroup.com>
2017-06-27 17:38:59 +0200

    Fix updateOutput for positional embeddings in CNN Encoder

--- onmt/modules/Position.lua
+++ onmt/modules/Position.lua
@@ -10,18 +10,24 @@ Parameters:
   * `dimension` - the dimension to be indexed.
 --]]
 
-function Position:__init(dimension)
+function Position:__init(dimension, max_pos)
    parent.__init(self)
    self.dimension = dimension
+   self.max_pos = max_pos
 end
 
 function Position:updateOutput(input)
    local dim = self.dimension < 0 and input:dim() + self.dimension + 1 or self.dimension
 
-   self.output = input
+   self.output = self.output:typeAs(input)
+   self.output:resizeAs(input)
 
    for i=1,self.output:size(dim) do
-     self.output:select(dim,i):fill(i)
+     if i <= self.max_pos then
+       self.output:select(dim,i):fill(i)
+     else
+       self.output:select(dim,i):fill(self.max_pos)
+     end
    end
 
    return self.output
