5ec8d3a1e686a1763eecf8f9bff3f998fcb55c0c
Jean A. Senellart <jean.senellart@systrangroup.com>
2017-06-30 09:37:55 +0200

    updateGradInput on CenteredWindow

--- onmt/modules/CenteredWindow.lua
+++ onmt/modules/CenteredWindow.lua
@@ -3,8 +3,9 @@
 local CenteredWindow, parent = torch.class('onmt.CenteredWindow', 'nn.Module')
 
 function CenteredWindow:__init(D)
-   parent.__init(self)
-   self.D=D
+  parent.__init(self)
+  self.D=D
+  self.gradInput = { torch.Tensor(), torch.Tensor() }
 end
 
 function CenteredWindow:updateOutput(input)
@@ -27,4 +28,29 @@ function CenteredWindow:updateOutput(input)
     self.output[i]:narrow(1, dec, right-left+1):copy(input[1][i]:narrow(1, left, right-left+1))
   end
   return self.output
+end
+
+function CenteredWindow:updateGradInput(input, gradOutput)
+  -- passing back gradient around the window
+  self.gradInput[1] = self.gradInput[1]:typeAs(gradOutput):resizeAs(input[1]):zero()
+  local size = input[1]:size()
+  local L = size[2]
+  for i = 1, input[2]:size(1) do
+    local left = math.floor(input[2][i] - self.D)
+    local dec = 1
+    if left < 1 then
+      dec = 2 - left
+      left = 1
+    end
+    local right = math.floor(input[2][i] + self.D)
+    if right > L then
+      right = L
+    end
+    self.gradInput[1][i]:narrow(1, left, right-left+1):copy(gradOutput[i]:narrow(1, dec, right-left+1))
+  end
+
+  -- no gradient on the positional tensor
+  self.gradInput[2] = self.gradInput[2]:typeAs(gradOutput):resizeAs(input[2]):zero()
+
+  return self.gradInput
 end
--- test/onmt/CenteredWindowTest.lua
+++ test/onmt/CenteredWindowTest.lua
@@ -20,4 +20,21 @@ function centeredWindowTest.forward()
   tester:eq(w[12][4]:sum(), 8)
 end
 
+function centeredWindowTest.forwardbackward()
+  local c = torch.Tensor(12, 12, 8):fill(1)
+  local p = torch.Tensor(12)
+  for i = 1, 12 do
+    p[i] = i
+  end
+  local g = torch.Tensor(12, 7, 8):fill(1)
+  local gI = onmt.CenteredWindow(3):backward({c, p}, g)
+
+  local gI1 = gI[1]
+  local gI2 = gI[2]
+  tester:eq(gI1[1]:narrow(1,1,4):sum(), 32)
+  tester:eq(gI1[1]:narrow(1,5,7):sum(), 0)
+  tester:eq(gI1[5]:sum(), 7*8)
+  tester:eq(gI2:sum(), 0)
+end
+
 return centeredWindowTest
