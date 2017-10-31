d51d8286b9382637016a95a13606f770f6ed9888
Jean A. Senellart <jean.senellart@systrangroup.com>
2017-10-31 08:01:00 +0100

    Deployed 823322d8116684c9b787437f52f338459387de4f to v0.8.0 with MkDocs 0.16.3 and mkultra 0.1.0.dev0

---
+++ v0.8.0/options/generate.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+gen_script_options ()
+{
+    echo "<!--- This file was automatically generated. Do not modify it manually but use the docs/options/generate.sh script instead. -->" > $2
+    echo "" >> $2
+    th $1 -h -md >> $2
+}
+
+gen_script_options preprocess.lua docs/options/preprocess.md
+gen_script_options train.lua docs/options/train.md
+gen_script_options translate.lua docs/options/translate.md
+gen_script_options tag.lua docs/options/tag.md
+gen_script_options tools/build_vocab.lua docs/options/build_vocab.md
+gen_script_options tools/release_model.lua docs/options/release_model.md
+gen_script_options tools/tokenize.lua docs/options/tokenize.md
+gen_script_options tools/learn_bpe.lua docs/options/learn_bpe.md
+gen_script_options tools/translation_server.lua docs/options/server.md
+gen_script_options tools/rest_translation_server.lua docs/options/rest_server.md
+gen_script_options tools/embeddings.lua docs/options/embeddings.md
+gen_script_options tools/average_models.lua docs/options/average_models.md
