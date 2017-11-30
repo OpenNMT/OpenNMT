OpenNMT/OpenNMT

commit 56466ee67da004c938fbcd22e5f7b87915ab1637

fix #444 (#446)
* fix #444 and add unit test

onmt/utils/Placeholders.lua
@@ -10,12 +10,11 @@ local function norm(t)
     end
     return v, vrep
   end
-  if t:sub(1, string.len('｟')) == '｟' then
+  if t:find('｟') then
     local p = t:find('｠')
     assert(p, 'invalid placeholder tag: '..t)
-    local tcontent = t:sub(string.len('｟')+1, p-1)
-    local fields = onmt.utils.String.split(tcontent, '：')
-    return '｟'..fields[1]..t:sub(p), fields[2] or fields[1]
+    local fields = onmt.utils.String.split(t, '：')
+    return fields[1]..t:sub(p), fields[2] or fields[1]
   end
   return t
 end

test/data/text-placeholder.tok
@@ -1,1 +1,2 @@
 Text with a placeholder ｟ent_url＃1：http://or1heron.cps.intel.com/cd/pvp/ef/ASMO-NA/ENG/download/234564.htm%09%09%09%09%09%09｠￭ 23 .
+i ￭｟TAB：%0009｠￭ Add the URL of the Canon license issuing server to [￭ Trusted sites ￭] ￭. (￭ P ￭. ｟bpt_CrossReference＃1：<CrossReference％0020markername="Add％0020the％0020URL％0020of％0020the％0020Canon％0020License％0020Issuing％0020Server"％0020markerid="10077612"％0020updatestate="0"％0020customstyleid=""％0020partnoid=""％0020referencefilename="\1T0000190473\Precautions_LicenseAuThentic_US.newt"％0020id="10085567"％0020style="1">｠￭ ｟ept_CrossReference＃1：</CrossReference>｠ ￭)

test/onmt/VocabularyTest.lua
@@ -75,6 +75,8 @@ end
 function vocabularyTest.placeholder()
   local vocabs = onmt.data.Vocabulary.init('source', testDataDir .. '/text-placeholder.tok', '', { 1000 }, { 1 }, '', noFilter, true)
   tester:assert(vocabs.words:lookup("｟ent_url＃1｠￭") ~= nil)
+  tester:assert(vocabs.words:lookup("￭｟TAB｠￭") ~= nil)
+  tester:assert(vocabs.words:lookup("｟ept_CrossReference＃1｠") ~= nil)
 end
 
 function vocabularyTest.initFeatures()

commit c95489b6bf2c47c0c137afb7f2362ed3fe8da59f

Fix regression introduced in 56466ee6
Also cleanup the function to avoid future logic error.

onmt/utils/Placeholders.lua
@@ -1,3 +1,5 @@
+local separators = require('tools.utils.separators')
+
 local function norm(t)
   if type(t) == "table" then
     local v = {}
@@ -10,13 +12,28 @@ local function norm(t)
     end
     return v, vrep
   end
-  if t:find('｟') then
-    local p = t:find('｠')
-    assert(p, 'invalid placeholder tag: '..t)
-    local fields = onmt.utils.String.split(t, '：')
-    return fields[1]..t:sub(p), fields[2] or fields[1]
+
+  local phStart = t:find(separators.ph_marker_open)
+
+  if not phStart then
+    return t
   end
-  return t
+
+  local phEnd = t:find(separators.ph_marker_close)
+  assert(phEnd, 'invalid placeholder tag: ' .. t)
+
+  local prefix = t:sub(1, phStart + separators.ph_marker_open:len() - 1)
+  local suffix = t:sub(phEnd)
+  local content = t:sub(prefix:len() + 1, phEnd - 1)
+  local fields = onmt.utils.String.split(content, '：')
+
+  if #fields == 1 then
+    return t, nil
+  else
+    local placeholder = fields[1]
+    local value = fields[2]
+    return prefix .. placeholder .. suffix, value
+  end
 end
 
 return {

test/data/text-placeholder.tok
@@ -1,2 +1,3 @@
 Text with a placeholder ｟ent_url＃1：http://or1heron.cps.intel.com/cd/pvp/ef/ASMO-NA/ENG/download/234564.htm%09%09%09%09%09%09｠￭ 23 .
 i ￭｟TAB：%0009｠￭ Add the URL of the Canon license issuing server to [￭ Trusted sites ￭] ￭. (￭ P ￭. ｟bpt_CrossReference＃1：<CrossReference％0020markername="Add％0020the％0020URL％0020of％0020the％0020Canon％0020License％0020Issuing％0020Server"％0020markerid="10077612"％0020updatestate="0"％0020customstyleid=""％0020partnoid=""％0020referencefilename="\1T0000190473\Precautions_LicenseAuThentic_US.newt"％0020id="10085567"％0020style="1">｠￭ ｟ept_CrossReference＃1：</CrossReference>｠ ￭)
+toto￨L ｟ept_AutoNumber＃1｠￭￨N toto￨L

test/onmt/VocabularyTest.lua
@@ -77,6 +77,7 @@ function vocabularyTest.placeholder()
   tester:assert(vocabs.words:lookup("｟ent_url＃1｠￭") ~= nil)
   tester:assert(vocabs.words:lookup("￭｟TAB｠￭") ~= nil)
   tester:assert(vocabs.words:lookup("｟ept_CrossReference＃1｠") ~= nil)
+  tester:assert(vocabs.words:lookup("｟ept_AutoNumber＃1｠￭") ~= nil)
 end
 
 function vocabularyTest.initFeatures()